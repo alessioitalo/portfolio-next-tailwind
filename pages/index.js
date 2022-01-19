494bd266361ffe2bb0f0635acc2b8b9cb38208d0
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-19 13:33:30 +0000

    link button to home

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
@@ -8,6 +10,8 @@ export default function Home() {
         <h4 className='text-4xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text '>
           I am a React Developer
         </h4>
+        <Link href='/work' passHref><button className="rounded p-3 mt-8 font-Roboto uppercase bg-myOrange text-slate-700 dark:bg-slate-700 dark:text-myOrange">See my work</button></Link>
+        <div></div>
       </div>
     </>
   );
