afa2588d81f9c09a6ba99fba055996971943da94
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-19 13:05:26 +0000

    dark mode added

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <>
       <div className='flex flex-col justify-center items-center min-h-screen'>
-        <h1 className='text-8xl font-bold uppercase bg-gradient-to-b from-myOrange via-myOrange to-myDark text-transparent bg-clip-text'>
+        <h1 className='text-8xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text'>
           Hi, I am Alessio
         </h1>
-        <h4 className='text-4xl font-bold uppercase bg-gradient-to-b from-myOrange via-myOrange to-myDark text-transparent bg-clip-text '>
+        <h4 className='text-4xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text '>
           I am a React Developer
         </h4>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        myDark: '#334756',
+        // myDark: '#334756',
         myOrange: '#FF4C29',
+        lightBgTop: '#F0F0F0',
+        lightBgBottom: '#E5E5E5',
+        darkBgTop: '#082032',
+        darkBgBottom: '#2c394b',
       },
       fontFamily: {
-        'Roboto': ['Roboto', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
