7e2f1372c6870229aec28c5afc4797d043b21e12
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-19 15:35:07 +0000

    hrefs added to main page social icons

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,10 @@ export default function Home() {
         <h1 className='text-8xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text'>
           Hi, I am Alessio
         </h1>
-        <h4 className='text-2xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text relative bottom-10'>...And I love {writer}</h4>
-      </div>  
-
+        <h4 className='text-2xl font-bold uppercase bg-gradient-to-b from-myOrange to-slate-700 text-transparent bg-clip-text relative bottom-10'>
+          ...And I love {writer}
+        </h4>
+      </div>
 
       <div className='flex flex-col justify-around items-center'>
         <Link href='/work' passHref>
@@ -38,10 +39,20 @@ export default function Home() {
         </Link>
 
         <div className='flex justify-center text-myOrange min-w-full relative top-10'>
-          <a href='' className='mx-4'>
+          <a
+            href='https://www.linkedin.com/in/alessiodalise/'
+            target='_blank'
+            rel='noreferrer'
+            className='mx-4'
+          >
             <BsLinkedin size={50} />
           </a>
-          <a href='' className='mx-4'>
+          <a
+            href='https://github.com/alessioitalo'
+            target='_blank'
+            rel='noreferrer'
+            className='mx-4'
+          >
             <BsGithub size={50} />
           </a>
         </div>
