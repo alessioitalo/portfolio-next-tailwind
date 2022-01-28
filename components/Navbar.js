4d3a0701cd6960f83e20126c4234d31a8e238eeb
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-28 08:52:58 +0000

    animated hamburger

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-// import { useState } from 'react';
+import MobileNav from './MobileNav';
+import { useState } from 'react';
 // import MobileNav from './MobileNav';
 
 const Navbar = () => {
   const router = useRouter();
-  // const [showMobile, setShowMobile] = useState(false);
+  const [showMobile, setShowMobile] = useState(false);
+
+  const showMobileHandler = () => {
+    setShowMobile((show) => !show);
+  };
 
   return (
     <nav className='text-orange-600 py-5 font-ubuntu'>
@@ -35,11 +40,27 @@ const Navbar = () => {
           )}
         </li>
       </ul>
-      <div className='md:hidden w-10 h-10 fixed top-6 right-5 cursor-pointer'>
-        <div className='bg-orange-600 w-10 h-1'></div>
-        <div className='bg-orange-600 w-10 h-1 relative top-1.5'></div>
-        <div className='bg-orange-600 w-10 h-1 relative top-3'></div>
+      <div
+        className='md:hidden w-10 h-10 fixed top-6 right-5 cursor-pointer'
+        onClick={showMobileHandler}
+      >
+        <div
+          className={`bg-orange-600 w-10 h-1 ${
+            showMobile && 'animatedHamburger'
+          }`}
+        ></div>
+        <div
+          className={`bg-orange-600 w-10 h-1 relative top-1.5 ${
+            showMobile && 'animatedHamburger'
+          }`}
+        ></div>
+        <div
+          className={`bg-orange-600 w-10 h-1 relative top-3 ${
+            showMobile && 'animatedHamburger'
+          }`}
+        ></div>
       </div>
+      {showMobile && <MobileNav setShowMobile={setShowMobile} />}
     </nav>
   );
 };
